c75cd51e0078f7baf08b13a124978985ee288618
Benny <83777519+bennyscripts@users.noreply.github.com>
2025-02-03 19:49:43 +0000

    Remove unnecessary create_defaults

--- utils/files.py
+++ utils/files.py
@@ -13,7 +13,3 @@ def resource_path(relative_path):
         return os.path.join(base_path, relative_path)
     else:
         return relative_path
-
-def create_defaults():
-    if not os.path.exists("scripts/"):
-        os.mkdir("scripts/")
