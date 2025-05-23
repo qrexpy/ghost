a9163e3ff58287ad955c6302457fc6725a2f8c15
Ben Tettmar <bentettmar@yahoo.com>
2025-05-23 15:14:07 +0100

    Change loading screen to be the same size as the main screen

--- gui/pages/loading.py
+++ gui/pages/loading.py
@@ -4,8 +4,6 @@ import ttkbootstrap as ttk
 class LoadingPage:
     def __init__(self, root):
         self.root = root
-        self.width = 400
-        self.height = 90
         
     def clear(self):
         for widget in self.root.winfo_children():
@@ -13,7 +11,7 @@ class LoadingPage:
         
     def draw(self, type="start"):
         loading_label = ttk.Label(self.root, text="Ghost is starting..." if type == "start" else "Ghost is restarting...", font=("Host Grotesk", 14 if sys.platform != "darwin" else 20, "bold"), anchor="center")
-        loading_label.pack(fill=ttk.BOTH, padx=30, pady=30, anchor="center")
+        loading_label.place(relx=0.5, rely=0.5, anchor="center")
         self.root.pack_propagate(False)
         
         return loading_label
